e253d6edb52207e3691ac9f571f6b707218c85d0
xxx <xxx@xxmail.com>
2019-11-17 16:32:09 +0800

    Update Useradd.java

--- WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/entity/Useradd.java
+++ WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/entity/Useradd.java
@@ -1,5 +1,71 @@
 package com.qst.ssm.entity;
 
 public class Useradd {
-    private Integer useraddId;
+    private Integer useraddId;          //用户地址ID
+    private Integer userId;              //用户ID
+    private  String addName;             //收货人
+    private  String addTelephone;        //收货人电话
+    private  String addAddress;          //详细地址
+
+    public Useradd() {
+    }
+
+    public Useradd(Integer useraddId, Integer userId, String addName, String addTelephone, String addAddress) {
+        this.useraddId = useraddId;
+        this.userId = userId;
+        this.addName = addName;
+        this.addTelephone = addTelephone;
+        this.addAddress = addAddress;
+    }
+
+    public Integer getUseraddId() {
+        return useraddId;
+    }
+
+    public void setUseraddId(Integer useraddId) {
+        this.useraddId = useraddId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getAddName() {
+        return addName;
+    }
+
+    public void setAddName(String addName) {
+        this.addName = addName;
+    }
+
+    public String getAddTelephone() {
+        return addTelephone;
+    }
+
+    public void setAddTelephone(String addTelephone) {
+        this.addTelephone = addTelephone;
+    }
+
+    public String getAddAddress() {
+        return addAddress;
+    }
+
+    public void setAddAddress(String addAddress) {
+        this.addAddress = addAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Useradd{" +
+                "useraddId=" + useraddId +
+                ", userId=" + userId +
+                ", addName='" + addName + '\'' +
+                ", addTelephone='" + addTelephone + '\'' +
+                ", addAddress='" + addAddress + '\'' +
+                '}';
+    }
 }
