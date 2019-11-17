b07ae7aaff7fdd0970616ea00164a2774ccfb9cb
Fallven <Fallven@outlook.com>
2019-11-17 14:37:23 +0800

    Update Product.java

--- WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/entity/Product.java
+++ WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/entity/Product.java
@@ -1,4 +1,12 @@
 package com.qst.ssm.entity;
 
 public class Product {
+    private Integer pdId;
+    private  Integer pdthreeId;
+    private String pdNo;
+    private  String pdName;
+    private Integer pdPrice;
+    private  Integer pdNumber;
+    private String pdCover;
+    
 }
