b6ba21384ec322b29c3111570950c8a1f38cf50a
xxx <xxx@xxmail.com>
2019-11-17 16:29:25 +0800

    Update Two.java

--- WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/entity/Two.java
+++ WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/entity/Two.java
@@ -1,4 +1,61 @@
 package com.qst.ssm.entity;
 
 public class Two {
+
+    private Integer pd_twoId;            //商品分类二级表ID
+    private  String pd_twoNo;            //商品分类二级NO
+    private  String pd_twoName;          //商品分类二级名字
+    private Integer claFid;              //商品分类ID
+
+    public Two() {
+    }
+
+    public Two(Integer pd_twoId, String pd_twoNo, String pd_twoName, Integer claFid) {
+        this.pd_twoId = pd_twoId;
+        this.pd_twoNo = pd_twoNo;
+        this.pd_twoName = pd_twoName;
+        this.claFid = claFid;
+    }
+
+    public Integer getPd_twoId() {
+        return pd_twoId;
+    }
+
+    public void setPd_twoId(Integer pd_twoId) {
+        this.pd_twoId = pd_twoId;
+    }
+
+    public String getPd_twoNo() {
+        return pd_twoNo;
+    }
+
+    public void setPd_twoNo(String pd_twoNo) {
+        this.pd_twoNo = pd_twoNo;
+    }
+
+    public String getPd_twoName() {
+        return pd_twoName;
+    }
+
+    public void setPd_twoName(String pd_twoName) {
+        this.pd_twoName = pd_twoName;
+    }
+
+    public Integer getClaFid() {
+        return claFid;
+    }
+
+    public void setClaFid(Integer claFid) {
+        this.claFid = claFid;
+    }
+
+    @Override
+    public String toString() {
+        return "Two{" +
+                "pd_twoId=" + pd_twoId +
+                ", pd_twoNo='" + pd_twoNo + '\'' +
+                ", pd_twoName='" + pd_twoName + '\'' +
+                ", claFid=" + claFid +
+                '}';
+    }
 }
