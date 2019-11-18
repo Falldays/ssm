0976504128f1209d42b1581769df2ec41387937c
Fallven <Fallven@outlook.com>
2019-11-18 10:32:05 +0800

    update

--- WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/dao/IProductDao.java
+++ WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/dao/IProductDao.java
@@ -1,4 +1,34 @@
 package com.qst.ssm.dao;
 
+import com.qst.ssm.entity.Product;
+
+import java.util.List;
+
 public interface IProductDao {
+    /**
+     *查询商品类全部信息
+     * @return list
+     */
+    List<Product> queryPro();
+    /**
+     * 根据商品ID查询信息
+     * @param pdId
+     * @return
+     */
+    Product getPro(int pdId);
+    /**
+     * 添加商品
+     * @param  product
+     * @return
+     */
+    int insertPro(Product product);
+
+    /**
+     * 根据商品ID删除商品
+     * @param
+     * @return
+     */
+    int deletePro(int pdId);
+
+
 }
--- WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/dao/impl/ProductImpl.java
+++ WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/dao/impl/ProductImpl.java
@@ -1,6 +1,28 @@
 package com.qst.ssm.dao.impl;
 
 import com.qst.ssm.dao.IProductDao;
+import com.qst.ssm.entity.Product;
+
+import java.util.List;
 
 public class ProductImpl implements IProductDao {
+    @Override
+    public List<Product> queryPro() {
+        return null;
+    }
+
+    @Override
+    public Product getPro(int pdId) {
+        return null;
+    }
+
+    @Override
+    public int insertPro(Product product) {
+        return 0;
+    }
+
+    @Override
+    public int deletePro(int pdId) {
+        return 0;
+    }
 }
