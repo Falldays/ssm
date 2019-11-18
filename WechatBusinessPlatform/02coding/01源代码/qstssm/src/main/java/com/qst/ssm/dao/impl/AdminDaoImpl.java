f7feb5982a0fb3578713eea9a09c5b9529a5bd0c
NalanJunwei <2874878492@qq.com>
2019-11-18 15:56:15 +0800

    1

---
+++ WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/dao/impl/AdminDaoImpl.java
@@ -0,0 +1,42 @@
+package com.qst.ssm.dao.impl;
+
+import com.qst.ssm.dao.IAdminDao;
+import com.qst.ssm.entity.Admin;
+import com.qst.ssm.mapper.AdminMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository()
+public class AdminDaoImpl implements IAdminDao {
+    @Autowired
+    @Qualifier("adminMapper")
+    private AdminMapper adminMapper;
+
+    @Override
+    public List<Admin> queryAdmin() {
+        return adminMapper.queryAdmin();
+    }
+
+    @Override
+    public Admin getAdmin(int adminId) {
+        return adminMapper.getAdmin(adminId);
+    }
+
+    @Override
+    public int insertAdmin(Admin admin) {
+        return adminMapper.insertAdmin(admin);
+    }
+
+    @Override
+    public int deleteAdmin(int adminId) {
+        return adminMapper.deleteAdmin(adminId);
+    }
+
+    @Override
+    public int updateAdmin(Admin admin) {
+        return adminMapper.updateAdmin(admin);
+    }
+}
--- WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/mapper/AdminMapper.java
+++ WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/mapper/AdminMapper.java
@@ -1,4 +1,50 @@
 package com.qst.ssm.mapper;
 
+import com.qst.ssm.entity.Admin;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 管理员表增、删、改、查Mapper接口
+ */
 public interface AdminMapper {
+    /**
+     * 查询所有管理员
+     *
+     * @return
+     */
+    List<Admin> queryAdmin();
+
+    /**
+     * 根据管理员ID加载管理员信息
+     * @param adminId
+     * @return
+     */
+    Admin getAdmin(@Param("admin_id") int adminId);
+
+    /**
+     * 添加管理员
+     *
+     * @param admin
+     * @return
+     */
+    int insertAdmin(Admin admin);
+
+    /**
+     * 根据管理员ID删除管理员记录
+     *
+     * @param adminId
+     * @return
+     */
+    int deleteAdmin(@Param("admin_id") int adminId);
+
+    /**
+     * 修改管理员
+     *
+     * @param admin
+     * @return
+     */
+    int updateAdmin(Admin admin);
+
 }
