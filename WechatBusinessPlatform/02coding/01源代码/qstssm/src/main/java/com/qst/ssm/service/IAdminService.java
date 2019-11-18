f93cdfdee9eacd446f2e09aaf8829b2c7587eca8
NalanJunwei <2874878492@qq.com>
2019-11-18 16:27:20 +0800

    2

--- WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/service/IAdminService.java
+++ WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/service/IAdminService.java
@@ -1,7 +1,45 @@
 package com.qst.ssm.service;
 
+import com.qst.ssm.entity.Admin;
+
+import java.util.List;
+
 /***
  * 管理员业务层接口
  */
 public interface IAdminService {
+    /**
+     * 查询所有管理员
+     *
+     * @return
+     */
+    List<Admin> queryAdmin();
+
+    /**
+     * 根据管理员ID加载管理员信息
+     * @param adminId
+     * @return
+     */
+    Admin getAdmin(int adminId);
+    /**
+     * 添加管理员
+     *
+     * @param admin
+     * @return
+     */
+    int insertEmp(Admin admin);
+
+    /**
+     * 根据管理员ID删除管理员记录
+     *
+     * @param adminId
+     * @return
+     */
+    int deleteAdmin(int adminId);
+
+    /**修改管理员
+     * @param admin
+     * @return
+     */
+    int updateAdmin(Admin admin);
 }
--- WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/service/impl/AdminServiceImpl.java
+++ WechatBusinessPlatform/02coding/01源代码/qstssm/src/main/java/com/qst/ssm/service/impl/AdminServiceImpl.java
@@ -1,4 +1,50 @@
 package com.qst.ssm.service.impl;
 
-public class AdminServiceImpl {
+
+import com.qst.ssm.dao.IAdminDao;
+import com.qst.ssm.entity.Admin;
+import com.qst.ssm.service.IAdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service("adminService")
+public class AdminServiceImpl implements IAdminService{
+    @Autowired
+    @Qualifier("adminDao")
+    private IAdminDao adminDao;
+
+    @Override
+    @Transactional(propagation = Propagation.NEVER )
+    public List<Admin> queryAdmin() {
+        return adminDao.queryAdmin();
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.NEVER )
+    public Admin getAdmin(int adminId) {
+        return adminDao.getAdmin(adminId) ;
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    public int insertEmp(Admin admin) {
+        return adminDao.insertAdmin(admin);
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    public int deleteAdmin(int adminId) {
+        return adminDao.deleteAdmin(adminId);
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    public int updateAdmin(Admin admin) {
+        return adminDao.updateAdmin(admin);
+    }
 }
